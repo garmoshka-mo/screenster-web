4f1bc6a089e5f929e6aa9d15832299e35115ea43
Daniel <daniel.kucherenko@agileengine.com>
2017-08-09 20:09:00 +0300

    login http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Screenster';
+  results: string[];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+
+    const body = 'email=daniel.kucherenko%40agileengine.com&password=qwe123&remember=on';
+    const options ={
+      headers: new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded')
+    };
+
+    // Make the HTTP request:
+    this.http.post('http://demo.screenster.io/authenticate/dosignin', body, options)
+    .subscribe(data => {
+      console.log(data);
+      // Read the result field from the JSON response.
+      this.results = data['results'];
+    });
+  }
+
 }
